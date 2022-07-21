4474571195e539159f6ffa69dc6a716d8f2eb686
MahdiSaadatiOrg <108065298+MahdiSaadatiOrg@users.noreply.github.com>
2022-07-21 14:30:27 +0430

    Add files via upload

---
+++ TamrinDr/Methods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TamrinDr
+{
+    public static class Methods
+    {
+
+        public static UInt64 CalPow(UInt64 a , UInt64 b) 
+        {
+            if (b == 0) return 1;
+
+            var result = a * CalPow(a,b-1);
+
+            return result;
+        }
+
+        public static UInt64 CalFac(UInt64 a)
+        {
+            if (a == 0) return 1;
+
+            var result = a * CalFac(a - 1);
+
+            return result;
+        }
+
+    }
+}
---
+++ TamrinDr/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TamrinDr;
+
+Console.WriteLine("Hello, World!");
+
+
+
+Console.WriteLine(Methods.CalPow(3,1));
+Console.WriteLine(Methods.CalFac(5));
